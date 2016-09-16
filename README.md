e6bac2939763408d54ada72eb08fd2bde630e442
Angie Smith <avksmit2@gmail.com>
2016-09-16 11:07:07 -0700

    initialize files and folders, including a README.md.

---
+++ README.md
@@ -0,0 +1,37 @@
+# _Address-book_
+
+#### _Web App to store contact addresses, September 9, 2016_
+
+#### By _**Angie Smith**_
+
+## Description
+
+_This website allows users to add contacts' addresses and contact information. The user can clear all of their contacts or search for a specific contact._
+
+## Specifications
+
+    * Site will allow the user to search for specific contacts by either the first or last name and display their information if there are any matching the search criterion or will display a message that there are no matches.
+
+## Setup/Installation Requirements
+
+* Clone the GitHub repository to your local machine
+* Make sure you have composer installed
+* Run composer install from the project root folder
+* Start php server from the web folder ( php -S localhost:8000 )
+* Go to localhost:8000 from your web browser
+
+## Known Bugs
+
+## Support and contact details
+
+_Contact avksmit2@gmail.com with any comments or questions_
+
+## Technologies Used
+
+_This project uses silex to create the framework, twig to create templates, and composer to manage project dependencies_
+
+### License
+
+*MIT License*
+
+Copyright (c) 2016 **_Angie Smith_**
---
+++ src/contacts.php
@@ -0,0 +1,115 @@
+<?php
+class Contact
+{
+    private $fName;
+    private $lName;
+    private $phone;
+    private $street;
+    private $city;
+    private $state;
+    private $zip;
+    private $img;
+
+    function __construct($fName, $lName, $email, $phone, $street, $city, $state, $zip, $img)
+    {
+        $this->fName = $fName;
+        $this->lName = $lName;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->street = $street;
+        $this->city = $city;
+        $this->state = $state;
+        $this->zip = $zip;
+        $this->img = $img;
+    }
+
+    function setFName($new_fName)
+    {
+        $this->fName = (string) $new_fName;
+    }
+    function setLName($new_lName)
+    {
+        $this->lName = (string) $new_lName;
+    }
+    function setEmail($new_email)
+    {
+        $this->email = (string) $new_email;
+    }
+    function setPhone ($new_phone)
+    {
+        $this->phone = (string) $new_phone;
+    }
+    function setStreet($new_street)
+    {
+        $this->street = (string) $new_street;
+    }
+    function setCity($new_city)
+    {
+        $this->city = (string) $new_city;
+    }
+    function setState($new_state)
+    {
+        $this->state = (string) $new_state;
+    }
+    function setZip($new_zip)
+    {
+        $this->zip = (int) $new_zip;
+    }
+    function setImg($new_img)
+    {
+        $this->img = (string) $new_img;
+    }
+
+    function getFName()
+    {
+        return $this->fName;
+    }
+    function getLName()
+    {
+        return $this->lName;
+    }
+    function getEmail()
+    {
+        return $this->email;
+    }
+    function getPhone()
+    {
+        return $this->phone;
+    }
+    function getStreet()
+    {
+        return $this->street;
+    }
+    function getCity()
+    {
+        return $this->city;
+    }
+    function getState()
+    {
+        return $this->state;
+    }
+    function getZip()
+    {
+        return $this->zip;
+    }
+    function getImg()
+    {
+        return $this->img;
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_contacts'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_contacts'];
+    }
+
+    static function clearAll()
+    {
+        return $_SESSION['list_of_contacts'] = array();
+    }
+}
+?>
